6ce99fa722831ed92cc4456984b205c075ab39b7
leoderoko <leoderoko@gmail.com>
2016-05-11 17:16:07 +0500

    Add sh tools

---
+++ scripts/upload-github-release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+VERSION=$(< ./VERSION)
+FILES=./release/*
+RELEASE_TYPE=$1
+
+function github_release() {
+    $GOPATH/bin/github-release "$@"
+}
+
+echo "--- Creating GitHub release v$VERSION"
+
+github_release release \
+    --user "leominov" \
+    --repo "commit-message-check" \
+    --tag "$VERSION" \
+    --name "$VERSION" \
+    --description "" \
+    $RELEASE_TYPE
+
+if [ $? -eq 0 ]; then
+    echo "Done."
+fi
+
+echo "--- Uploading files for release v$VERSION"
+
+for fullfile in $FILES
+do
+    filename=$(basename "$fullfile")
+    echo $filename
+
+    github_release upload \
+        --user "leominov" \
+        --repo "commit-message-check" \
+        --tag "$VERSION" \
+        --name "$filename" \
+        --file "$fullfile"
+
+    if [ $? -eq 0 ]; then
+        echo "Done."
+    fi
+done
