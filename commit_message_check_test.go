9ed4c8296620c2c3e19a12129ad70f5df9d519c9
leoderoko <leoderoko@gmail.com>
2016-05-11 17:00:30 +0500

    Update test cases

--- commit_message_check_test.go
+++ commit_message_check_test.go
@@ -2,12 +2,27 @@ package main
 
 import "testing"
 
-type Test struct {
+type TestMessage struct {
 	in  string
 	out error
 }
 
-var tests = []Test{
+type TestBool struct {
+	in  string
+	out bool
+}
+
+var testsDot = []TestBool{
+	{"Abc.", true},
+	{"Abc", false},
+}
+
+var testsUpper = []TestBool{
+	{"First char is upper", true},
+	{"first char is not upper", false},
+}
+
+var testsMessage = []TestMessage{
 	{"One line message.", errSubjectRequired},
 	{"two line message with a long-long-long-long-long-long-long-long-long-long-long subject\n\nanother line", errSubjectTooLong},
 	{"two line message\n\nanother line", errSubjectWrongCase},
@@ -18,35 +33,25 @@ var tests = []Test{
 }
 
 func TestIsEndWithDot(t *testing.T) {
-	str := "Abc."
-	result := IsEndWithDot(str)
-	if result != true {
-		t.Errorf("IsEndWithDot(%s) == %v, want %v", str, result, false)
-	}
-
-	str = "Abc"
-	result = IsEndWithDot("Abc")
-	if result != false {
-		t.Errorf("IsEndWithDot(%s) == %v, want %v", str, result, false)
+	for _, test := range testsDot {
+		result := IsEndWithDot(test.in)
+		if result != test.out {
+			t.Errorf("%v want %v", result, test.out)
+		}
 	}
 }
 
 func TestIsFirstCharUpper(t *testing.T) {
-	str := "First char is upper"
-	result := IsFirstCharUpper(str)
-	if result != true {
-		t.Errorf("IsFirstCharUpper(%s) == %v, want %v", str, result, false)
-	}
-
-	str = "first char is not upper"
-	result = IsFirstCharUpper(str)
-	if result != false {
-		t.Errorf("IsFirstCharUpper(%s) == %v, want %v", str, result, false)
+	for _, test := range testsUpper {
+		result := IsFirstCharUpper(test.in)
+		if result != test.out {
+			t.Errorf("%v want %v", result, test.out)
+		}
 	}
 }
 
 func TestCommitMessageCheck(t *testing.T) {
-	for _, test := range tests {
+	for _, test := range testsMessage {
 		result := CommitMessageCheck(test.in)
 		if result != test.out {
 			t.Errorf("IsFirstCharUpper(%s) == %v, want %v", test.in, result, test.out)
